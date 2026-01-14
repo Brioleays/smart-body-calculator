38aa76e659fbdc1a107e92cf771c0f308a361f8c
Olakunde-Samuel2020 <olakundesamuel30@gmail.com>
2026-01-14 06:35:34 +0100

    smart body edits

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 // supabase.js
-const SUPABASE_URL = "https://fwclvonnragrmzeeqyll.supabase.co";
-const SUPABASE_ANON_KEY =  "sb_publishable_eYSzoFnGDB8m_YYCSCGqbA_RtJgD5Sy";
+const SUPABASE_URL = "https://dautyurfgvyenuegcjps.supabase.co";
+const SUPABASE_ANON_KEY =  "sb_publishable_1wsugr9SntYndCpXUJlGMQ_hq-DdUeI";
 
 window.supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
